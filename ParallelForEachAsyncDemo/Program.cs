a9a29d48d8710f276cfca9db8ecfbfd1ee50e39d
Jaganmohanreddy <jaganmohan.mukkala@gmail.com>
2021-12-28 20:24:25 +0530

    Add project files.

---
+++ ParallelForEachAsyncDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+var userHandlers = new[]
+{
+    "users/mukkalajagan",
+    "users/okyrylchuk",
+    "users/shanselman",
+    "users/jaredpar",
+    "users/davidfowl"
+};
+
+using HttpClient client = new HttpClient();
+client.BaseAddress = new Uri("https://api.github.com");
+client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("DotNet", "6"));
+
+ParallelOptions parallelOptions = new()
+{
+    MaxDegreeOfParallelism = 3
+};
+
+await Parallel.ForEachAsync(userHandlers, parallelOptions, async (uri, token) =>
+{
+    var user = await client.GetFromJsonAsync<GitHubUser>(uri, token);
+
+    Console.WriteLine($"Name: {user.Name}\nBio: {user.Bio}\n");
+});
+Console.ReadLine();
+public class GitHubUser
+{
+    public string Name { get; set; }
+    public string Bio { get; set; }
+}
+
+//public record GitHubUser(string Name, string Bio);
